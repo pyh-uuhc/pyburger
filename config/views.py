f6159749dbfcdb58311c6e18d822bd7704b8883e
youngha <pyh.uuhc@gmail.com>
2024-08-02 18:36:32 +0900

    update View 'burger_list'

--- config/views.py
+++ config/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
+from burgers.models import Burger
+
 
 def main(request):
     return render(request, 'main.html')
 
 
 def burger_list(request):
-    return render(request, 'burger_list.html')
+    burgers = Burger.objects.all()
+    print('전체 햄버거 목록:', burgers)
+
+    context = {
+        'burgers': burgers,
+    }
+    return render(request, 'burger_list.html', context)
